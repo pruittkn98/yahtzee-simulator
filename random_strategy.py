0c715b8b00cc6ed84e91bd4bd4d0d0e2a7d090fe
Katie Pruitt <pruittkn@gmail.com>
2025-11-18 10:35:41 +0700

    saved runs locally

--- random_strategy.py
+++ random_strategy.py
@@ -1,11 +1,6 @@
 from yahtzee import Game
-from strategy_utils import score_all_rolls
-from constants import CATEGORIES, DEFAULT_TIE_BREAK_ORDER, ALL_ROLLS
-from itertools import combinations_with_replacement, combinations, product
-from collections import Counter
+from constants import CATEGORIES, DEFAULT_TIE_BREAK_ORDER
 import numpy as np
-import pandas as pd
-from tqdm import tqdm
 
 class RandomStrategy():
     def __init__(self, start_seed=15, rng_seed = 1, tiered_strategy = True):
@@ -14,21 +9,25 @@ class RandomStrategy():
         self.rng = np.random.default_rng(seed=rng_seed)
         self.tiered_strategy = tiered_strategy
 
-    def choose_dice_to_keep(self, game_state: dict, tiered_strategy = True):
+    def choose_dice_to_keep(self):
         """
         Choose number and combination of dice to keep at random
-        -- Tiered random strategy: Choose # of dice to keep and then choose which dice to keep
+        -- Tiered random strategy: Choose whether to turn game, choose # of dice to keep and then choose which dice to keep
         -- Purely random strategy: Choose which dice to keep completely at random
         """
 
         # Choose number of dice
-        if tiered_strategy:
-            num_dice =  int(np.ceil(self.rng.random() * 6)) - 1
-            if num_dice == 5:
-                return []
+        if self.tiered_strategy:
+            # 50/50 chance of ending each turn
+            end_turn = self.rng.integers(0, 2)
+            if end_turn == 1:
+                return list(range(0, 5))
             
-            # Choose dice combination
-            return self.rng.choice(range(0, 5), num_dice)
+            # 1/5 chance of each remaining # of dice
+            num_keep_dice =  self.rng.integers(0, 4)
+            
+            # Choose dice combinations
+            return self.rng.choice(range(0, 5), num_keep_dice)
 
         else:
             best_keep_mask = [self.rng.choice([0, 1]) for i in range(0, 5)]
@@ -65,7 +64,7 @@ class RandomStrategy():
         return score_idxs[0][0], True
 
 
-    def choose_bonus_category(self, game_state: dict, category: str, best_score_idx: int, available_categories: list, tie_break_order_idx=DEFAULT_TIE_BREAK_ORDER):
+    def choose_bonus_category(self, game_state: dict, category: str, best_score_idx: int, available_categories: list):
         '''
         Choose bonus category if player rolls second Yahtzee
         Force Joker rules: Search upper section first. If not filled, apply to best lower section option
@@ -111,7 +110,7 @@ class RandomStrategy():
                 if self.game.is_turn_over() == True:
                     break
                 
-                best_keep = self.choose_dice_to_keep(self.game.get_game_state())
+                best_keep = self.choose_dice_to_keep()
                 # End turn if best option is to keep all five dice
                 if len(best_keep) == 5:
                     break
@@ -125,5 +124,5 @@ class RandomStrategy():
         return self.game.get_game_state()
 
 if __name__ == '__main__':
-    r = RandomStrategy(start_seed=15, rng_seed = 1)
+    r = RandomStrategy(start_seed=16, rng_seed = 1, tiered_strategy=True)
     print(r.run_strategy())
